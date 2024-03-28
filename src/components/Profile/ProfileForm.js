36f3a536e5ffec639bb4a44e298f21bc5e2bc537
YAMITRAO <work.aky@gmail.com>
2024-03-28 14:06:33 +0530

    old password change done

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,42 @@
+import { useContext, useRef } from 'react';
 import classes from './ProfileForm.module.css';
+import DataContext from '../../Context/DataContext';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const ProfileForm = () => {
+  const changePasswordRef = useRef();
+  const ctx = useContext(DataContext);
+  const hist = useHistory();
+
+  
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let newUpdatedPassword = changePasswordRef.current.value;
+    let token = ctx.userAuthDetails.idToken;
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCTtOX2Di6R4YAhCh2Lnm63plJ7z5rYEr8',{
+      method:'POST',
+      body: JSON.stringify({
+        idToken: token,
+        password: newUpdatedPassword,
+        returnSecureToken: false,
+      }),
+      headers:{
+        'Content-type':"application/json"
+      }
+    }).then( res => {
+      if(res.ok){
+        hist.push("/");
+      }
+      return res.json().then( data => console.log(data))
+    })
+
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={ submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength='7' ref={changePasswordRef}/>
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -1,13 +1,24 @@
+import { useContext } from 'react';
 import ProfileForm from './ProfileForm';
 import classes from './UserProfile.module.css';
+import DataContext from '../../Context/DataContext';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const UserProfile = () => {
-  return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-    </section>
-  );
+  const hist = useHistory();
+  const ctx = useContext(DataContext);
+  if(ctx.isAuth){
+    return (
+      <section className={classes.profile}>
+        <h1>Your User Profile</h1>
+        <ProfileForm />
+      </section>
+    );
+  }
+  else{
+    hist.push('/')
+  }
+  
 };
 
 export default UserProfile;
